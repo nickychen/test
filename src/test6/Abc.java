c442203096935ca3a536c02ef4ca63aa171ea025
nickychen <snc21@163.com>
2020-11-09 22:09:55 +0800

    create project

---
+++ src/test6/Abc.java
@@ -0,0 +1,14 @@
+package test6;
+
+public class Abc {
+
+	public Abc() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
